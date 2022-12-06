8ee5411d66cf4728801db8943679fd27db349316
requ <azizmailcom1910@gmail.com>
2022-12-06 13:42:16 +0600

    greetings

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ loer[0] = prompt('what is your name?', '');
 loer[1] = prompt('what is your last name?', '');
 loer[2] = prompt('how old are you?', '');
 
+
+const ler = 'hoys';
+console.log(`https//requs.kg/${ler}/ancord`);
+
+const user = 'Requiem';
+alert(`Приветствую, ${user}`);
+
+const lol = 'baber';
+console.log(`https//quyer.com/${lol}/yesrit`)
+
+const lastName = 'Enigame';
+alert(`Вот и ты!, ${lastName}`)
